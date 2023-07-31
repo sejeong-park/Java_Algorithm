3f09bd130e89c54b141ea1210e28d2b5799158ed
SejeongPark <62873925+sejeong-park@users.noreply.github.com>
2023-08-01 01:08:19 +0900

    [D1] Title: 스탬프 찍기, Time: 133 ms, Memory: 20,412 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // 주석이.. 필요 없는 길이..
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i =0 ; i<N;i++){
+            System.out.print("#");
+        }
+    }
+}
