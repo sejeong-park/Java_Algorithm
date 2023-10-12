c5257783b6c1e65d980ffebd515549997ab26925
SejeongPark <62873925+sejeong-park@users.noreply.github.com>
2023-10-12 16:18:08 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -15,14 +15,3 @@ from used_goods_user
 inner join user 
 on used_goods_user.user_id = user.writer_id
 order by user_id desc;
-
-# SELECT user_id
-#      , nickname
-#      , CONCAT(city, " ", street_address1, " ", street_address2) AS address
-#      , CONCAT(LEFT(tlno,3),"-",SUBSTR(tlno,4,4),"-",RIGHT(tlno,4)) AS tlno
-# FROM used_goods_user
-# WHERE user_id IN (SELECT writer_id
-#                   FROM used_goods_board
-#                   GROUP BY writer_id
-#                   HAVING COUNT(*)>=3)
-# ORDER BY user_id DESC
