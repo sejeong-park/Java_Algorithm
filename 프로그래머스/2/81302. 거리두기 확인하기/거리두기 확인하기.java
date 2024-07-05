c78daefcd0fb38c16c2d752ee1e69a31f0ff0df2
SejeongPark <62873925+sejeong-park@users.noreply.github.com>
2024-07-05 17:01:31 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.30 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -6,58 +6,57 @@ import java.awt.*;
 3. 단, 응시자가 앉아 있는 자리 사이 파티션으로 막혀있는 경우는 허용
 */
 class Solution {
-    
+
     static char [][] map;
+    static boolean [][] visited;
+
     static final char STUDENT = 'P', WALL = 'X', EMPTY = 'O';
-    
+
     static int [] deltaRow = {-1, 1, 0, 0};
     static int [] deltaCol = {0, 0, -1, 1};
-    
+
     public static boolean inMap(int row, int col) {
         return row >= 0 && row < 5 && col >= 0 && col < 5;
     }
-    
+
     public static int getDistance(int r1, int c1, int r2, int c2) {
         return Math.abs(r1 - r2) + Math.abs(c1 - c2);
     }
-    
+
     public static boolean bfs(Point current) {
-        // current는 비교할 초기 응시자 포인트임!
-        
         boolean [][] visited = new boolean[5][5];
-        
+
         Deque<Point> queue = new ArrayDeque<>();
         queue.add(current);
         visited[current.x][current.y] = true;
-        
+
         while(!queue.isEmpty()) {
-            
+
             Point now = queue.poll();
-            
-            // 만약 거리두기 내의 범위에 다른 응시자가 있다면? -> 자기 자신 제외
-            if (map[now.x][now.y] == STUDENT && now.x != current.x && now.y != current.y) {
-                if (getDistance(now.x, now.y, current.x, current.y)<=2) {
-                    return false;
-                } 
-            }
-            
-            
+
             for (int direction = 0; direction < 4; direction ++) {
                 int nextRow = now.x + deltaRow[direction];
                 int nextCol = now.y + deltaCol[direction];
-                
+
                 if (!inMap(nextRow, nextCol)) continue;
                 if (visited[nextRow][nextCol] || map[nextRow][nextCol] == WALL) continue;
-                // 체크
-                queue.add(new Point(nextRow, nextCol));
-                visited[nextRow][nextCol] = true;
+
+                int distance = getDistance(current.x, current.y, nextRow, nextCol);
+
+                if (map[nextRow][nextCol] == STUDENT && distance <= 2) {
+                    return false;
+                } 
+                else if (map[nextRow][nextCol] == EMPTY && distance < 2) {
+                    queue.add(new Point(nextRow, nextCol));
+                    visited[nextRow][nextCol] = true;
+                }
             }
         }
-        
+
         return true;
     }
-    
-    
+
+
     public static int checkWaitingRoom(String [] place) {
         // 맵 만들기
         ArrayList<Point> student = new ArrayList<>(); // 응시자 들어있는 맵
@@ -70,24 +69,22 @@ class Solution {
                 }
             }
         }
-        
-        int result = 1;
+
         for (Point person : student) {
             boolean flag = bfs(person);
             if (!flag) {
-                result = 0;
-                return result;
+                return 0;
             }
         }
-        return result;
+        return 1;
     }
-    
+
     public int[] solution(String[][] places) {
         int[] answer = new int[5]; // 대기실 5개
         for (int idx = 0; idx < 5; idx ++) {
             answer[idx] = checkWaitingRoom(places[idx]);
         }
-        
+
         return answer;
     }
 }
