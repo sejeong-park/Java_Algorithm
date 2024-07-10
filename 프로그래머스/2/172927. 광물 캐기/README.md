e6148bfc3e554a12eea0dda850053db5e7785d58
SejeongPark <62873925+sejeong-park@users.noreply.github.com>
2024-07-10 18:01:54 +0900

    [level 2] Title: 광물 캐기, Time: 6.78 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 1.54 ms
+메모리: 78.5 MB, 시간: 6.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 10일 04:19:18
+2024년 07월 10일 18:01:52
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,79 +1,115 @@
 import java.util.*;
+/**
+광물 캐기
+다이아몬드 곡괭이, 철 곡괭이, 돌 곡괭이를 0 ~ 5개 가지고 있음
+곡괭이 광물 캘 때 피로도 소모
+
+최소한의 피로도 광물을 캐려고 한다.
+1. 사용할 수 있는 곡괭이 중 아무거나 하나를 선택해 광물을 캔다.
+2. 한번 사용한 곡괭이는 사용할 수 없을 때까지 사용
+3. 광물은 주어진 순서대로만 캘 수 있다.
+4. 광산에 모든 광물을 캐거나 더 사용할 곡괭이가 없을 때까지 광물을 캔다.
+
+곡괭이 하나를 선택해 광물 5개를 연속으로 캐고, 다음 곡괭이를 선택해 광물 5개를 연속으로 캐는 과정 반복
+더 사용할 곡괭이가 없거나 광산에 있는 모든 광물 캘때까지 과정 반복
+*/
 
 class Solution {
+    
+    // 곡괭이 개수
+    static int [] picks;
+    
+    // 광물의 피로드
+    static int [][] stress = {{1,1,1}, {5,1,1}, {25,5,1}};
+    
+    static int minStress;
+    
+    static final int DIA = 0;
+    static final int IRON = 1;
+    static final int STONE = 2;
+    
+    static ArrayList<int []> array = new ArrayList<>();
+    
+    boolean check() {
+        int cnt = 3;
+        for (int idx = 0; idx < 3; idx ++) {
+            if (picks[idx] == 0) {
+                cnt -=1;
+            }
+        }
+        if (cnt == 0) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+    
+    void dfs(int depth, int totalStress) {
+        
+        // 모든 탐색 시 종료
+        if (depth == array.size()) {
+            minStress = Math.min(minStress, totalStress);
+            return;
+        }
+        
+        // 곡괭이를 모두 소진하면 종료
+        if (!check()) {
+            minStress = Math.min(minStress, totalStress);
+            return;
+        }
+        
+        // 곡괭이 별 탐색
+        for (int idx = 0; idx < 3; idx ++) {
+            int [] stressCnt = array.get(depth); // 곡괭이 별 스트레스 정도
+            // 만약 곡괭이가 남아있다면
+            if (picks[idx] > 0) {
+                picks[idx] -= 1;
+                totalStress += stressCnt[idx];
+                dfs(depth + 1, totalStress);
+                totalStress -= stressCnt[idx];
+                picks[idx] += 1;
+            }
+        }
+        return;
+    }
+    
     public int solution(int[] picks, String[] minerals) {
-		int answer = 0;
-
-		int[][] arr = new int[][]{{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
-		int size = Arrays.stream(picks).sum();
-		List<Mineral> list = new ArrayList<>();
-
-		for(int i = 0; i < minerals.length; i+=5) {
-			if(size == 0) {
-				break;
-			}
-
-			int diamond = 0;
-			int iron = 0;
-			int stone = 0;
-
-			for(int j = i; j < i + 5; j++) {
-				if(j == minerals.length) {
-					break;
-				}
-
-				int mineral = 0;
-				if(minerals[j].startsWith("i")) {
-					mineral = 1;
-				} else if(minerals[j].startsWith("s")) {
-					mineral = 2;
-				}
-
-				diamond += arr[0][mineral];
-				iron += arr[1][mineral];
-				stone += arr[2][mineral];
-			}
-			list.add(new Mineral(diamond, iron, stone));
-			size--;
-		}
-
-		list.sort(((o1, o2) -> (o2.stone - o1.stone)));
+        int answer = 0;
         
-        for(Mineral m : list) {
-			int diamond = m.diamond;
-			int iron = m.iron;
-			int stone = m.stone;
-
-			if(picks[0] > 0) {
-				answer += diamond;
-				picks[0]--;
-				continue;
-			}
-
-			if(picks[1] > 0) {
-				answer += iron;
-				picks[1]--;
-				continue;
-			}
-
-			if(picks[2] > 0) {
-				answer += stone;
-				picks[2]--;
-			}
-		}
+        this.picks = picks;
+        minStress = Integer.MAX_VALUE;
+        // 곡괭이는 5개기준으로 소요된다.
+        for (int idx = 0; idx < minerals.length; idx += 5) {
+            int diaStress = 0, ironStress = 0, stoneStress = 0; 
+            for (int mineralIdx = idx; mineralIdx < idx + 5; mineralIdx ++) {
+                
+                if (mineralIdx == minerals.length) {
+                    break;
+                }
+                
+                // 광물 확인
+                int mineral = -1; 
+                if (minerals[mineralIdx].equals("diamond")) {
+                    mineral = 0;
+                } else if (minerals[mineralIdx].equals("iron")) {
+                    mineral = 1;
+                } else {
+                    mineral = 2;
+                }
+                
+                // 광물에 스트레스 지수 추가
+                diaStress += stress[DIA][mineral];
+                ironStress += stress[IRON][mineral];
+                stoneStress += stress[STONE][mineral];  
+            }
+            // System.out.println(diaStress + " " + ironStress + " " + stoneStress);
+            array.add(new int [] {diaStress, ironStress, stoneStress});
+        }
         
-		return answer;
-	}
+        System.out.println("size :: " + array.size());
 
-	class Mineral {
-		private int diamond;
-		private int iron;
-		private int stone;
-
-		public Mineral(int diamond, int iron, int stone) {
-			this.diamond = diamond;
-			this.iron = iron;
-			this.stone = stone;
-		}
-	}
+        dfs(0,0);
+        
+        return minStress;
+    }
 }
