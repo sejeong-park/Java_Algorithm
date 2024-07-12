1d15d701a1015a56b41f1205a246e7dca0d27e88
SejeongPark <62873925+sejeong-park@users.noreply.github.com>
2024-07-12 17:49:15 +0900

    [level 2] Title: 점 찍기, Time: 0.13 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -10,10 +10,10 @@ k, d가 주어질 때 점을 찍으려한다.
 class Solution {
     public long solution(long k, long d) {
         long answer = 0;
-
-        for(long x = 0; x <= d; x += k){
-            long maxY = (long) Math.sqrt(d*d - x*x); 
-            answer += maxY / k + 1;
+        
+        for (long x = 0; x <= d; x += k) {
+            long max = (long) Math.sqrt(d * d - x * x); // y의 최댓값
+            answer += max / k + 1;
         }
         
         return answer;
