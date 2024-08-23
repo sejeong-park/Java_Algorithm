f4408b5b66b459c0dbc8252b844abc3656f3cb72
SejeongPark <62873925+sejeong-park@users.noreply.github.com>
2024-08-24 02:18:08 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 9.15 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -12,11 +12,12 @@ class Solution {
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]); 
         // 소요 시간 기준 오름차순 정렬
         PriorityQueue<int []> queue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]); // 시간 기준
-        
+
         int index = 0;
         int end = 0;
         int total = 0;
         int count = 0;
+        
         while (count < jobCnt) {
             
             // index 기준 pq에 넣기
@@ -24,9 +25,12 @@ class Solution {
                 queue.add(jobs[index++]);
             }
             
+            // 큐에 작업이 없다면, 작업 요청시점이 가장 빠른 작업 추가
             if (queue.isEmpty()) {
                 end = jobs[index][0];
-            } else {
+            } 
+            // 큐에 작업이 있다면, 작업 소요시간이 가장 빠른 작업 수행
+            else {
                 int [] now = queue.poll();
                 total += now[1] + end - now[0];
                 end += now[1]; // 시간 
